bacb4e61b3733d436fc4ca830083d3119e3b4755
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-04-21 11:27:58 +0300

    added a function  with a switch statment to  determine the winner based on the options picked

--- src/components/Game/Game.component.js
+++ src/components/Game/Game.component.js
@@ -12,11 +12,57 @@ const Game = ({ myChoice, Score, setScore }) => {
 
     useEffect(() => {
         housePick()
+
     }, [])
+
+
+
+    const Results = () => {
+
+
+        switch (myChoice + house) {
+            case "scissorspaper":
+            case "rockscissors":
+            case "paperrock":
+                setWinner("win");
+                setScore(Score + 1)
+
+                break;
+
+            case "paperscissors":
+            case "scissorsrock":
+            case "rockpaper":
+                setWinner("loose");
+                setScore(Score - 1)
+
+                break;
+            case "paperpaper":
+            case "scissorsscissors":
+            case "rockrock":
+                setWinner("draw")
+
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+
+    useEffect(() => {
+        Results(myChoice, house)
+    })
+
     return (
         <div className={classes.game}>
             myChoice: {myChoice} <br />
             house choice:{house}<br />
+
+            Results :{winner === 'win' && <h2> You  Win</h2>}
+            {winner === 'loose' && <h2> You  Loose</h2>}
+            {winner === 'draw' && <h2>         It's a tie!</h2>}
+
 
         </div>
     )
