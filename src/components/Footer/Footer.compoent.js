939a8fa344e49a741764a723b50ac63a734a0532
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-04-21 16:33:29 +0300

    added a footer and modal which is displayed on clicking the rules button

--- src/components/Footer/Footer.compoent.js
+++ src/components/Footer/Footer.compoent.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import './Footer.module.css'
+import React, { useState } from 'react'
+import Modal from '../Modal/Modal.component'
+import classes from './Footer.module.css'
+
 const Footer = () => {
+
+    const [modal, setModal] = useState(false)
+
+    const toggle = () => {
+        setModal(!modal)
+        console.log('click')
+    }
     return (
-        <div>Footer</div>
+        <>
+            <footer className={classes.footer}>
+                <p>
+
+                    Coded By mcwachira
+                </p>
+                <button className={classes.rules} onClick={toggle}>
+
+                    Rules
+                </button>
+
+            </footer>
+            {modal ? <Modal toggle={toggle} /> : null}
+
+        </>
+
     )
 }
 
-export default Footer
+export default Footer;
--- src/components/Modal/Modal.component.js
+++ src/components/Modal/Modal.component.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import './Modal.module.css'
+import ReactDOM from 'react-dom'
+import classes from './Modal.module.css'
+import CloseButton from '../../assets/icon-close.svg'
+import ModalImage from '../../assets/image-rules.svg'
 
-const Modal = () => {
-    return (
-        <div>Modal</div>
+const Modal = ({ toggle }) => {
+    return ReactDOM.createPortal(
+        <div className={classes.modalContainer}>
+            <div className={classes.modalBox}>
+                <div className={classes.modalHeader}>
+
+                    <h1>
+                        Rules
+                    </h1>
+                    <button className={classes.close} onClick={toggle}>
+                        <img src={CloseButton} alt="close button" />
+                    </button>
+                </div>
+
+                <img src={ModalImage} alt="modal " />
+            </div>
+        </div>,
+        document.getElementById('modal')
     )
 }
 
